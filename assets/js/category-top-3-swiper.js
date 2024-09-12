a469d14caec6a6e0c85f5e2209dd1a5892098f30
annnnangan <anna.ymngan@gmail.com>
2024-09-13 01:33:43 +0800

    Feature - review expense - expense rank

---
+++ assets/js/category-top-3-swiper.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".category-top-3-swiper", {
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-custom-next",
+    prevEl: ".swiper-button-custom-prev",
+  },
+});
