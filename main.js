e0436f5c7948ed6780e5dbd22373f176f148dd9f
annnnangan <anna.ymngan@gmail.com>
2024-09-15 01:06:28 +0800

    Add index header and pop up modal

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".header");
+  const pageTitleElement = document.getElementById("pageTitle");
+  const page = header.getAttribute("data-page");
 
-document.addEventListener('DOMContentLoaded', function () {
-    const header = document.querySelector('.header');
-    const pageTitleElement = document.getElementById('pageTitle');
-    const page = header.getAttribute('data-page');
-
-    // 將 page 參數直接設為標題
-    pageTitleElement.textContent = page.charAt(0).toUpperCase() + page.slice(1);
+  // 將 page 參數直接設為標題
+  pageTitleElement.textContent = page.charAt(0).toUpperCase() + page.slice(1);
 });
